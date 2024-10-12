1a53639109ebe4614a72d91fa161cf62607fe57f
yyin <yyin>
2024-10-12 09:44:26 +0800

    update with data query

--- infra/alb.tf
+++ infra/alb.tf
@@ -32,7 +32,7 @@ resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-12345678"
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_lb_listener" "app_listener" {
